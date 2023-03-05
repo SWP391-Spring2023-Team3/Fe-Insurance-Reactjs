3d2a04ff5f493023cc5621d62ecd2c3fb1c808fb
phương nguyễn <nguyenphuong113yb@gmail.com>
2023-03-05 21:27:10 +0700

    Update

--- src/components/Layout/App.js
+++ src/components/Layout/App.js
@@ -11,6 +11,7 @@ import Login from '../Login/login.js';
 import UserProfile from '../User/UserProfile.js';
 import UserList from '../Admin/UserList.js';
 import Adminsite from '../Admin/Adminsite.js';
+import Payment from '../User/Payment.js';
 
 
 
@@ -26,6 +27,7 @@ function App() {
                     <Route path="/profile" element={<UserProfile />} />
                     <Route path="/list-user" element={<UserList />} />
                     <Route path="/adminsite" element={<Adminsite />}/>
+                    <Route path="/payment"  element={<Payment/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
